5c8717ce322e288da3d88a528b220695b8f03673
sheilacazrod <sheilacazorla124@gmail.com>
2024-01-19 15:56:40 +0000

    first commit

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Client client = new Client(1, "Sheila", "sheila@gmail.com");
+        System.out.println(client.toString());
+    }
+}
